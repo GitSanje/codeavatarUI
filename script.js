8a5f5b5e64f2536c95ceb5cf11b9a5aa515f34f4
Sanjay <karkisanjay2002@gmail.com>
2026-01-07 22:21:30 +0545

    added slider functionality using js

--- script.js
+++ script.js
@@ -1,21 +1,75 @@
-const dots = document.querySelectorAll('.dot');
-const prev = document.getElementById('prev');
-const next = document.getElementById('next');
+document.addEventListener("DOMContentLoaded", () =>{
+  const slides = document.querySelectorAll(".slider-content");
+  const prevBtn = document.getElementById("prevBtn");
+  const nextBtn = document.getElementById("nextBtn");
+    const dots = document.querySelectorAll(".slider-controls .dot");
+  const slider = document.querySelector(".slider");
 
-let activeIndex = 0;
 
-function updateDots() {
-  dots.forEach((dot, i) => {
-    dot.classList.toggle('active', i === activeIndex);
-  });
-}
 
-next.addEventListener('click', () => {
-  activeIndex = (activeIndex + 1) % dots.length;
-  updateDots();
-});
+  let currentIndex =0;
+  let interval = null;
+   const AUTO_SLIDE_DELAY = 5000;
 
-prev.addEventListener('click', () => {
-  activeIndex = (activeIndex - 1 + dots.length) % dots.length;
-  updateDots();
-});
+
+     /* ---------- CORE FUNCTIONS ---------- */
+     function showSlide(index){
+         slides.forEach(slide => slide.classList.remove("active"));
+         dots.forEach(dot => dot.classList.remove("dot-active"));
+        slides[index].classList.add("active");
+        dots[index].classList.add("dot-active");
+        currentIndex = index;
+     }
+
+    function nextSlide() {
+      showSlide((currentIndex + 1) % slides.length);
+    }
+
+    function prevSlide() {
+      showSlide(
+        (currentIndex - 1 + slides.length) % slides.length
+      );
+    }
+
+    /* ---------- BUTTON EVENTS ---------- */
+  nextBtn.addEventListener("click",() =>{
+    nextSlide()
+    restartAutoSlide();
+  })
+   prevBtn.addEventListener("click",() =>{
+    prevSlide()
+    restartAutoSlide();
+  })
+
+   /* ---------- DOT EVENTS ---------- */
+
+  dots.forEach((dot,index )=> {
+     dot.addEventListener("click", () => {
+       showSlide(index)
+       restartAutoSlide();
+    })
+  })
+
+   /* ---------- AUTOPLAY ---------- */
+  function startAutoSlide() {
+    interval = setInterval(nextSlide, AUTO_SLIDE_DELAY);
+    }
+  function stopAutoSlide() {
+    clearInterval(interval);
+  }
+  function restartAutoSlide() {
+    stopAutoSlide();
+    startAutoSlide();
+  }
+ 
+    /* ---------- PAUSE ON HOVER ---------- */
+  slider.addEventListener("mouseenter", stopAutoSlide);
+  slider.addEventListener("mouseleave", startAutoSlide);
+
+
+  /* ---------- INIT ---------- */
+  showSlide(0);
+  startAutoSlide();
+
+
+})
